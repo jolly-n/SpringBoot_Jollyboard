0df314a701f8358ceea2a02c28578dc0dfc5b9e4
yujeong lim <anneu5712@gmail.com>
2022-03-30 14:52:01 +0900

    init

---
+++ src/test/java/jolly/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package jolly.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
